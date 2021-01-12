eb659188d2bf6e3adbb93a9e64034d541bf518e3
451360508@qq.com <451360508@qq.com>
2021-01-12 15:40:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tron/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.tron;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.tron.protos.Contract;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
